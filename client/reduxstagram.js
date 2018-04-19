c9a3ad9f5319e56724ba91f1f41d896a1ff318ef
Nick Quan <nickkquan@gmail.com>
2018-04-19 13:14:05 -0700

    added App component, connecting actions and mapping tstate to Main

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -6,21 +6,25 @@ import css from './styles/style.styl';
 
 // Import Components
 
-import Main from './components/main';
+import App from './components/app';
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
 // Import React Router Dependencies
 
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
 
 const router = (
-	<Router history={browserHistory}>
-		<Route path="/" component={Main}>
-			<IndexRoute component={PhotoGrid} />
-			<Route path="/view/:postId" component={Single} />
-		</Route>
-	</Router>
+	<Provider store={store}>
+		<Router history={history}>
+			<Route path="/" component={App}>
+				<IndexRoute component={PhotoGrid} />
+				<Route path="/view/:postId" component={Single} />
+			</Route>
+		</Router>
+	</Provider>
 );
 
 render(router, document.getElementById('root'));
