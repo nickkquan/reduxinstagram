d20d2b518cdcf0a55829d31b2da556fb81573e97
Nick Quan <nickkquan@gmail.com>
2018-04-19 14:20:52 -0700

    created new comment component

--- client/components/single.js
+++ client/components/single.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Photo from './photo';
+import Comments from './comments';
 
 const Single = React.createClass({
 	render() {
+		const index = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+		const post = this.props.posts[index];
 		return (
 			<div>
-				<div className="single-photo">I'm the single grid.</div>
+				<div className="single-photo">
+					<Photo index={index} post={post} {...this.props} />
+					<Comments />
+				</div>
 			</div>
 		);
 	}
